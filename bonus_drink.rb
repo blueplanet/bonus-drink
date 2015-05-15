78d44efb450eebaf9a5e7448a719ca155eb858d5
blueplanet <erguolinge@gmail.com>
2015-05-16 07:27:01 +0900

    Add answer

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,14 @@
 class BonusDrink
-  def self.total_count_for(amount)
-    0
+  def initialize(drinks)
+    @drinks = drinks
+  end
+
+  def total_count
+    sum(@drinks, 0)
+  end
+
+  def sum(drinks, blank_pins)
+    all_pins = drinks + blank_pins
+    all_pins < 3 ? drinks : drinks + sum(all_pins / 3, all_pins % 3)
   end
 end
--- spec/bonus_drink_spec.rb
+++ spec/bonus_drink_spec.rb
@@ -1,5 +1,9 @@
 require File.expand_path(File.dirname(__FILE__) + '/../bonus_drink')
 
 describe BonusDrink do
-  specify { expect(BonusDrink.total_count_for(0)).to eq 0 }
+  it { BonusDrink.new(0).total_count.should eq 0 }
+  it { BonusDrink.new(1).total_count.should eq 1 }
+  it { BonusDrink.new(3).total_count.should eq 4 }
+  it { BonusDrink.new(11).total_count.should eq 16 }
+  it { BonusDrink.new(100).total_count.should eq 149 }
 end
